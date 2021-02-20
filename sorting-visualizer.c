d34dbabad0eada4d58f64e2d6fb58f9c0ec561d7
Jake Goodman <jakewgoodman@gmail.com>
2021-02-19 19:33:37 -0500

    added command line params

--- sorting-visualizer.c
+++ sorting-visualizer.c
@@ -1,20 +1,25 @@
 // This program visualizes sorting algorithms
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include "sorts.c"
 
-#define N 28
-#define WINDOW_HEIGHT 25
+#define N 35
+#define WINDOW_HEIGHT 30
 #define ARRAY_HEIGHT 20
-#define DELAY 15
+#define DELAY 10
 
 void visualize_array(int *arr);
 void delay(int ml);
 
 
-int main(void) {	
+/* Accepts exactly one command line argument which tells the program what sort to
+   visualize. Can be one one: "insertion"..., */
+int main(int argc, char *argv[]) {	
+	assert(argc == 2);
 	int arr[N];
 	for (int i = 0; i < N; i++)
 		arr[i] = rand() % ARRAY_HEIGHT;
@@ -22,10 +27,18 @@ int main(void) {
 	int arr_cpy[N];
 	for (int i = 0; i < N; i++)
 		arr_cpy[i] = arr[i];
+
+	struct queue *animations = NULL;
+	char *cmd = argv[1];
+	if (strcmp(cmd, "insertion") == 0)
+		animations = insertion_sort_animations(arr_cpy, N);
+	else {
+		printf("Invalid sort: %s\n", cmd);
+		exit(EXIT_SUCCESS);
+	}
+
 	visualize_array(arr);
 	delay(DELAY);
-
-	struct queue *animations = insertion_sort_animations(arr_cpy, N);
 	while (!queue_is_empty(animations)) {
 		visualize_array(arr);
 		struct pair *to_swap = pop(animations);
